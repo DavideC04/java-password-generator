aab8c9e848046d4bb92d19c4ac5a812e1950b94c
Davide Cangiano <cangiano.0497@gmail.com>
2023-07-12 15:03:43 +0200

    Exercise done

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,51 @@
+package org.lessons.java.security;
+
+public class PasswordGenerator {
+
+    public static void main(String[] args) {
+
+        // Dichiaro le variabili
+
+        String userName;
+        String userSurname;
+        String favoriteColor;
+
+        // Data di nascita
+
+        int dayOfBirth;
+        int monthOfBirth;
+        int yearOfBirth;
+
+        // Inserisco un valore alle variabili
+
+        userName = "Davide";
+        userSurname ="Cangiano";
+        favoriteColor ="Rosso";
+        dayOfBirth = 7;
+        monthOfBirth = 4;
+        yearOfBirth = 1997;
+
+        // Dichiaro ed inizializzo una variabile String che contenga la data
+        // giorno / mese / anno
+
+        String dateOfBirth = dayOfBirth + "/" + monthOfBirth + "/" + yearOfBirth;
+
+        // Dichiaro una variabile che contenga la somma dell'anno di nascita
+
+        int datePassword = dayOfBirth + monthOfBirth + yearOfBirth;
+
+        // Dichiaro una variabile che contenga la password generate dalla somma delle variabili
+
+        String passwordGenerate = userName + "-" +  userSurname + "-" +  favoriteColor + "-" + datePassword;
+
+        // Stampo in console
+
+        System.out.println("Il tuo nome: " + userName);
+        System.out.println("Il tuo cognome: " + userSurname);
+        System.out.println("Il tuo colore preferito: " + favoriteColor);
+        System.out.println("La tua data di nascita: " + dateOfBirth);
+        System.out.println("La password generata è: " + passwordGenerate);
+
+
+    }
+}
